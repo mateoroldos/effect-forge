c1678ea8cf5de2cd4021544819a135f9f258e212
Mateo Roldos <mateoroldos19@gmail.com>
2026-09-17 10:40:57 -0300

    refactor(web): isolate PostgreSQL resource setup

---
+++ apps/web/src/lib/server/postgres.ts
@@ -0,0 +1,46 @@
+import { PersistencePostgres } from "@effect-forge/database-postgres";
+import * as PgClient from "@effect/sql-pg/PgClient";
+import { Context, Effect, Layer, Redacted, Schema } from "effect";
+import { Client } from "pg";
+
+export class AuthClient extends Context.Service<AuthClient, Client>()(
+  "@effect-forge/web/Postgres/AuthClient",
+) {}
+
+class AuthConnectionError extends Schema.TaggedError<AuthConnectionError>()(
+  "AuthConnectionError",
+  {},
+) {}
+
+export const authLayer = (connectionString: string) =>
+  Layer.effect(
+    AuthClient,
+    Effect.acquireRelease(
+      Effect.sync(
+        () =>
+          new Client({
+            connectionString,
+            types: PersistencePostgres.typeParsers,
+          }),
+      ),
+      (client) => Effect.promise(() => client.end()).pipe(Effect.timeoutOption(1000)),
+    ).pipe(
+      Effect.tap((client) =>
+        Effect.tryPromise({
+          try: () => client.connect(),
+          catch: () => new AuthConnectionError(),
+        }).pipe(Effect.orDie),
+      ),
+    ),
+  );
+
+export const sqlLayer = (connectionString: string) =>
+  PgClient.layerFrom(
+    PgClient.makeClient({
+      url: Redacted.make(connectionString),
+      acquireForStream: false,
+      types: PersistencePostgres.typeParsers,
+    }),
+  );
+
+export * as Postgres from "./postgres.ts";
